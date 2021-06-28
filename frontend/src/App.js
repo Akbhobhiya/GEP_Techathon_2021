db4701bde52102203a330584059a227af6314a5e
MD MUDASSIR ALI <mma10@iitbbs.ac.in>
2021-06-28 17:33:00 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+// import components
+import Home from './components/home'
+
+function App() {
+    return (
+      <div className="App">
+        <Home/>
+      </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/home.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class home extends Component{
+
+    render(){
+        return(
+            <div>
+                <p>welcome</p> 
+            </div>
+        )
+    }
+}
+
+export default home;
